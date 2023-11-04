2b56436a93608de447ecc0f4131d69221dc82431
dattakale21 <dattakale2008@gmail.com>
2023-11-04 09:55:26 +0530

    some changes

---
+++ Frontend/Datta/project1.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".wrapper");
+const registerlink = document.querySelector(".register-link");
+const loginlink = document.querySelector(".login-link");
+const forgotPasswordLink = document.querySelector(".forgot a");
+
+wrapper.classList.remove('active');
+
+// Functions:-
+registerlink.onclick = () => {
+    wrapper.classList.add('active'); // Show the registration form
+}
+
+loginlink.onclick = () => {
+    wrapper.classList.remove('active'); // Hide the registration form, show the login form
+}
+
+forgotPasswordLink.onclick = () => {
+    wrapper.classList.add('active'); // Show the registration form
+}
+
+
+
+// // /* 15-08-2023 */
+// // /* --------------------- END ------------------------ */
+
